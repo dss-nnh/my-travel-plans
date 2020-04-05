ab8170b5e8c81ee0617a16468c8a487ba5976dd5
dss-nnh <62170162+dss-nnh@users.noreply.github.com>
2020-04-05 23:28:04 +0800

    my-travel-plans fourth commit

--- 第一次提交/生命游戏.cpp
+++ 第一次提交/生命游戏.cpp
@@ -1,14 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <windows.h>
+#include<time.h>
 
-//Ï¸°û¹æÔò
-int map[50][50];
-// living 1;
-// death 0;
+#define ROWS 50
+#define COLS 50
+bool map[ROWS][COLS];
 
 
-int Count_cellsNumber(int xline,int yline)
+int count_cellsNumber(int xline,int yline)
 {
 	int living_cellsnumber=0,i,j;
 	for(i=xline-1;i<=xline+1;i++){
@@ -21,10 +21,11 @@ int Count_cellsNumber(int xline,int yline)
 	return living_cellsnumber;
 }
 
+
 void cells_state(int xline,int yline)
 {
 	int living_cellsnumber=0;
-	living_cellsnumber=Count_cellsNumber(xline,yline);
+	living_cellsnumber=count_cellsNumber(xline,yline);
 	if((living_cellsnumber>=4)&&(map[xline][yline]==1))
 		map[xline][yline]=0;
 	else if((living_cellsnumber==3)&&(map[xline][yline]==0))
@@ -32,57 +33,49 @@ void cells_state(int xline,int yline)
 	else if(living_cellsnumber==2);
 	else if(((living_cellsnumber==1)||(living_cellsnumber==0))&&(map[xline][yline]==1))
 		map[xline][yline]=0;
+}	
 
-}
 
 
-void Run_game()
+void run_game()
 {
 	int i,j;
 	while(1){
 		system("cls");
-		for(j=1;j<50;j++){
-			for(i=1;i<50;i++){
+		for(j=1;j<ROWS;j++){
+			for(i=1;i<COLS;i++){
 				if(map[i][j]==1)
-					printf("¨€");
+					printf("â—");
 				else
-					printf("  ");
+					printf("â—‹");
 				if(i==49)
 					printf("\n");
 			}
 		}
-		for(j=1;j<50;j++){
-			for(i=1;i<50;i++){
+		for(j=1;j<ROWS;j++){
+			for(i=1;i<COLS;i++){
 				cells_state(i,j);
 			}
 		}
-		Sleep(10);
+		Sleep(1);
+		//getchar();
 	}
 }
 
 void Date()
 {
 	int i,j;
-	map[5][5]=1;
-	map[5][6]=1;
-	map[4][4]=1;
-	map[4][14]=1;
-	map[5][15]=1;
-	map[5][16]=1;
-
-
-	for(i=0;i<50;i++){
-		for(j=0;j<50;j++){
-			if(map[i][j]!=1)
-				map[i][j]=0;
-		}
+	srand((unsigned)time(NULL));
+	for(i=0;i<ROWS;i++){
+		for(j=0;j<COLS;j++)
+			map[i][j]=rand()%2;
 	}
 }
 
 
 void main()
 {
-	system("mode con cols=99 lines=50");
+	system("mode con cols=100 lines=50");
 	Date();
-	Run_game();
+	run_game();
 }
